d5d4c67434de1ed617abe4eb84e508983d387866
hejianxin <hejianxinsir@gmail.com>
2020-03-22 09:27:34 +0800

    初次提交代码

---
+++ pages/history/history.js
@@ -0,0 +1,23 @@
+
+const app = getApp()
+
+Page({
+  data: {
+    history: []
+  },
+
+  onShow: function(){
+    this.setData({ history: wx.getStorageSync('history') })
+  },
+
+  onTapItem: function(e){
+    wx.reLaunch({
+      url: `/pages/index/index?query=${e.currentTarget.dataset.query}`
+    })
+  },
+
+  onTapDelete: function(){
+    wx.setStorageSync('history', [])
+    this.setData({ history: [] })
+  }
+})
